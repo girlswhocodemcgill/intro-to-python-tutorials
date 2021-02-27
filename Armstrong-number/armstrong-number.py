9d465113b6c118f567eee65d91ba7d459d24248b
Delal Tomruk <66208179+delaltomruk@users.noreply.github.com>
2021-02-27 14:14:32 +0300

    Add files via upload

---
+++ Armstrong-number/armstrong-number.py
@@ -0,0 +1,16 @@
+number = input("Enter a number: ")
+digit_nums = len(number) # input are strings!
+number = int(number)
+tmp = number
+sum = 0
+
+while(tmp > 0):
+    last_digit = tmp % 10
+    sum += last_digit ** digit_nums
+    tmp = tmp // 10 
+
+
+if(number == sum):
+    print("Yes!")
+else:
+    print("No!")
