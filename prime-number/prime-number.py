f15f81a82e2a026639a9e3aa34decae06520f8f2
Delal Tomruk <66208179+delaltomruk@users.noreply.github.com>
2021-02-27 14:19:57 +0300

    Delete prime-number directory

--- prime-number/prime-number.py
+++
@@ -1,9 +0,0 @@
-A prime number is a natural number greater than 1 that is not a product of two smaller natural numbers. 
-A natural number greater than 1 that is not prime is called a composite number.
-
-2 is the smallest prime number since it's factors are 2 and 1.
-11 is a prime number since it's factors are 1 and 11.
-
-But 4 is a composite number since it's factors are 1, 2, and 4.
-
-Write a program that gets an input from the user and checks if the number is prime or not.
